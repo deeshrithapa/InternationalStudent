53653a7155899f4d71f51b22200b4838fdd9a128
deeshrithapa <deeshri.t@gmail.com>
2026-03-24 22:37:17 +0000

    path fix

--- js/script.js
+++ js/script.js
@@ -1,23 +1,7 @@
-// Load navbar
-
-fetch("components/header.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("header").innerHTML = data;
-
-    // Menu toggle after header loads
-    const toggle = document.getElementById("menuToggle");
-    const nav = document.getElementById("navLinks");
-
-    toggle.addEventListener("click", () => {
-      nav.classList.toggle("show");
-    });
-  });
 
 // Load Footer
-
-fetch("../components/footer.html")
-.then(response => response.text())
-.then(data => {
-document.getElementById("footer").innerHTML = data;
-});
+fetch("/components/footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
